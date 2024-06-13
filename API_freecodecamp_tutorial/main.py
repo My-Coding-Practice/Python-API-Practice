4f1a144993cbbd9c5635a1ab39edbd9124a4cd34
Greg Babbert <gdbabbert@gmail.com>
2024-06-12 23:06:24 -0400

    made more progress on video

--- API_freecodecamp_tutorial/main.py
+++ API_freecodecamp_tutorial/main.py
@@ -10,6 +10,8 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 0}, {"title": "favorite foods", "content": "I like pizza", "id": 1}]
+
 @app.get("/")  # This is called a decorator
 def root():
     return {"message": "Hello World"}
@@ -22,8 +24,9 @@ def get_posts():
 # def create_posts(payload: dict = Body(...)): # dict is the data type being passed in, payload is what it is called, Body is from fastapi
 #     print(payload)
 #     return {"new_post": f"title {payload['title']} content: {payload['content']}"}
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(new_post: Post):
     print(new_post)
+    print('hi', new_post.model_dump())
     return {"data": "new post"}
 # title str, content str
--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ### This repository will be used to cover the freecodecamp tutorial video, plus any additional API practice I kight want to do.
 
 ##### Video link: https://www.youtube.com/watch?v=0sOvCWFmrtA&t=11s
-##### Left video at: 1:28:55
+##### Left video at: 1:32:34
 
 ##### f"Hello {variable}" is the same as `Hello ${variable}` in JavaScript
